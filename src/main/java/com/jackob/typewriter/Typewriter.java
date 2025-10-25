bcbad7393e8b170282d1a48d08cea00a5abf50b4
Jackob2004 <zjakub2004@gmail.com>
2025-10-25 13:38:46 +0200

    Initial commit

---
+++ src/main/java/com/jackob/typewriter/Typewriter.java
@@ -0,0 +1,12 @@
+package com.jackob.typewriter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Typewriter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}

7210f7647ee8b9c82b0ea868d6b5a3f1cdec0205
Jackob2004 <zjakub2004@gmail.com>
2025-10-26 20:38:39 +0100

    Register commands and init manager
    Wire everyting pu in the main class for testing purposes.

--- src/main/java/com/jackob/typewriter/Typewriter.java
+++ src/main/java/com/jackob/typewriter/Typewriter.java
@@ -1,12 +1,28 @@
 package com.jackob.typewriter;
 
+import com.jackob.typewriter.commands.TestCommand;
+import com.jackob.typewriter.commands.TestRemoveCommand;
+import com.jackob.typewriter.manager.HologramManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Typewriter extends JavaPlugin {
 
+    private HologramManager manager;
+
     @Override
     public void onEnable() {
+        this.manager = new HologramManager();
 
+        registerCommand("writer", new TestCommand(this));
+        registerCommand("remove", new TestRemoveCommand(manager));
     }
 
+    @Override
+    public void onDisable() {
+        if (manager != null) {
+            manager.unregisterAllDisplays();
+        }
+    }
+
+    public HologramManager getManager() { return manager; }
 }

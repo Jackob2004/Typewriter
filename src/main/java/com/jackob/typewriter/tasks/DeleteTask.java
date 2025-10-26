7d9b4a8f3561b97af5f2a31e356d9e574c285da1
Jackob2004 <zjakub2004@gmail.com>
2025-10-26 14:33:34 +0100

    Implement delete task
    Quickly deletes whole text.

---
+++ src/main/java/com/jackob/typewriter/tasks/DeleteTask.java
@@ -0,0 +1,35 @@
+package com.jackob.typewriter.tasks;
+
+import com.jackob.typewriter.Typewriter;
+import com.jackob.typewriter.objects.AnimationContext;
+import com.jackob.typewriter.utils.WriterUtil;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class DeleteTask implements WriterTask {
+
+    private final Typewriter plugin;
+
+    public DeleteTask(Typewriter plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public BukkitTask execute(Runnable onComplete, AnimationContext context) {
+        return new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                if (context.getCurrCharacter() == 0) {
+                    this.cancel();
+                    onComplete.run();
+                    return;
+                }
+
+                context.decrementCurrCharacter();
+                context.getDisplay().text(WriterUtil.generateTextComponent(context.getCurrText(), context.getCurrCharacter()));
+            }
+
+        }.runTaskTimer(plugin, 20, 2);
+    }
+}

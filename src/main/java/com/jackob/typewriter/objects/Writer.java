bb8def8793437e55738a664ee9c492b255c1feef
Jackob2004 <zjakub2004@gmail.com>
2025-10-26 15:40:36 +0100

    Implement sequential tasks execution

--- src/main/java/com/jackob/typewriter/objects/Writer.java
+++ src/main/java/com/jackob/typewriter/objects/Writer.java
@@ -2,6 +2,9 @@ package com.jackob.typewriter.objects;
 
 import com.jackob.typewriter.Typewriter;
 import com.jackob.typewriter.tasks.*;
+import com.jackob.typewriter.utils.WriterUtil;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TextDisplay;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -12,15 +15,44 @@ public class Writer {
 
     private final Queue<WriterTask> tasks;
 
+    private final int maxWordLength;
+
+    private AnimationContext context;
+
     Writer(Typewriter plugin, Builder builder) {
         this.plugin = plugin;
         this.tasks = builder.tasks;
+        this.maxWordLength = builder.maxWordLength;
+    }
+
+    public void start(Player player) {
+        final String invisiblePart = "W".repeat(this.maxWordLength + 4);
+        final TextDisplay background = WriterUtil.spawnDisplay(player, true, invisiblePart);
+        final TextDisplay display = WriterUtil.spawnDisplay(player, false, "");
+
+        context = new AnimationContext(display, maxWordLength);
+
+        plugin.getManager().registerDisplay(background);
+        plugin.getManager().registerDisplay(display);
+
+        executeNext();
+    }
+
+    private void executeNext() {
+        if (tasks.isEmpty()) {
+            return;
+        }
+
+        tasks.poll().execute(plugin, this::executeNext, context);
     }
 
     public static class Builder {
         private final Queue<WriterTask> tasks = new LinkedList<>();
 
+        private int maxWordLength = 0;
+
         public Builder type(String text) {
+            maxWordLength = Math.max(maxWordLength, text.length());
             tasks.offer(new TypingTask(text));
             return this;
         }

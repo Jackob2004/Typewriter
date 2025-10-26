09a738022cbd07e20db1fbc9cb32a5a016a7c20f
Jackob2004 <zjakub2004@gmail.com>
2025-10-26 15:40:02 +0100

    Add spawn display utility method

--- src/main/java/com/jackob/typewriter/utils/WriterUtil.java
+++ src/main/java/com/jackob/typewriter/utils/WriterUtil.java
@@ -1,7 +1,13 @@
 package com.jackob.typewriter.utils;
 
 import net.kyori.adventure.text.Component;
+import org.bukkit.Color;
+import org.bukkit.Location;
+import org.bukkit.entity.Display;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TextDisplay;
 
+import static net.kyori.adventure.text.format.NamedTextColor.BLACK;
 import static net.kyori.adventure.text.format.NamedTextColor.GREEN;
 
 public class WriterUtil {
@@ -20,6 +26,29 @@ public class WriterUtil {
         }
 
         return Component.text(sb.toString()).color(GREEN);
+    }
+
+    /**
+     *
+     * @param player the display will be shown to
+     * @param isBackground will only act as a background
+     * @param text determines how big the display is
+     * @return spawned display
+     */
+    public static TextDisplay spawnDisplay(Player player, boolean isBackground, String text) {
+        final Location location = player.getEyeLocation().add(player.getLocation().getDirection().multiply(2));
+        final Component component = Component.text(text).color(BLACK);
+
+        return player.getWorld().spawn(location, TextDisplay.class, entity -> {
+            entity.text(component);
+            entity.setBillboard(Display.Billboard.VERTICAL);
+
+            if (isBackground) {
+                entity.setBackgroundColor(Color.BLACK);
+            } else {
+                entity.setSeeThrough(true);
+            }
+        });
     }
 
 }

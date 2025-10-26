f8662c9dfa128c3eef4ac35379836fad3f2ea86a
Jackob2004 <zjakub2004@gmail.com>
2025-10-26 20:33:47 +0100

    Modify animation context
    Add receiver of the animation to the context.

--- src/main/java/com/jackob/typewriter/objects/AnimationContext.java
+++ src/main/java/com/jackob/typewriter/objects/AnimationContext.java
@@ -1,17 +1,21 @@
 package com.jackob.typewriter.objects;
 
+import org.bukkit.entity.Player;
 import org.bukkit.entity.TextDisplay;
 
 public class AnimationContext {
 
     private final TextDisplay display;
 
+    private final Player receiver;
+
     private int currCharacter;
 
     private final char[] currText;
 
-    public AnimationContext(TextDisplay display, int textLength) {
+    public AnimationContext(TextDisplay display, Player receiver, int textLength) {
         this.display = display;
+        this.receiver = receiver;
         this.currCharacter = 0;
         this.currText = new char[textLength];
     }
@@ -21,6 +25,8 @@ public class AnimationContext {
     public TextDisplay getDisplay() { return display; }
 
     public char[] getCurrText() { return currText; }
+
+    public Player getReceiver() { return receiver; }
 
     public void incrementCurrCharacter() {
         if (currCharacter + 1 < currText.length) {
